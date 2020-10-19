48252d73a8b1c3fd38ec0c8da2adebb816a4a9db
MasatoMakino <unagiinu.g@gmail.com>
2020-10-19 13:08:07 +0900

    update : gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,13 +1,13 @@
 const { parallel, series } = require("gulp");
 
-const doc = require("gulptask-tsdoc").get();
-const server = require("gulptask-dev-server").get("./docs/demo");
-const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
-  externalScripts: []
+const doc = require("gulptask-tsdoc").generateTask();
+const server = require("gulptask-dev-server").generateTask("./docs/demo");
+const { bundleDemo, watchDemo } = require("gulptask-demo-page").generateTasks({
+  externalScripts: [],
 });
 
 const { tsc, tscClean, watchTsc } = require("gulptask-tsc").get({
-  projects: ["tsconfig.json", "tsconfig.esm.json"]
+  projects: ["tsconfig.cjs.json", "tsconfig.esm.json"],
 });
 
 const watchTasks = async () => {
