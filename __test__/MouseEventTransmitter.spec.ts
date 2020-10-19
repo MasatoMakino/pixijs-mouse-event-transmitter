1ea930317193d417ff1530c279c6f7c97ab9cad3
MasatoMakino <unagiinu.g@gmail.com>
2020-10-19 14:52:58 +0900

    change : jest-canvas-mock to @jest-runner/electron

--- __test__/MouseEventTransmitter.spec.ts
+++ __test__/MouseEventTransmitter.spec.ts
@@ -1,3 +1,12 @@
+import { Application } from "pixi.js";
+
 describe("MouseEventTransmitter", () => {
+  const app = new Application({
+    width: 640,
+    height: 480,
+    backgroundColor: 0x666666,
+  });
+  const view = app.view;
+
   test("init", () => {});
 });
